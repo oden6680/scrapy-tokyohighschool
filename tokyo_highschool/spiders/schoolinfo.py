34dd1f346a991847dae44559735ef697b8b75a7c
oden6680 <oden6680@gmail.com>
2022-07-19 12:06:33 +0900

    スクレイピング

---
+++ tokyo_highschool/spiders/schoolinfo.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class SchoolinfoSpider(scrapy.Spider):
+    name = 'schoolinfo'
+    allowed_domains = ['www.minkou.jp']
+    start_urls = ['https://www.minkou.jp/hischool/search/pref=tokyo/']
+
+    def parse(self, response):
+        for href in response.css('#container > #contents > #main > #under > div.mod-listSearch > ul > li > a::attr(href)'):
+            url = 'https://www.minkou.jp//hischool/school/university' + href.get().replace("/hischool/school","")
+            yield scrapy.Request(url, callback=self.parse_item)
+
+    def parse_item(self, response):
+        yield {
+            'highschool':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-top > div.mod-school-name::text').get(),
+            'schoolcaption':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-top > div.mod-school-caption::text').get().replace(/(|)/g,""),
+            'hensa':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-bottom > div.mod-school-info > p > span::text').get(),
+        }
