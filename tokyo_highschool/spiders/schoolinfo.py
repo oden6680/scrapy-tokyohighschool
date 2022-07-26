c70b5e8ee0f9167d09e3a8c35cea7881af6efcc8
oden6680 <oden6680@gmail.com>
2022-07-26 11:55:04 +0900

    みん高から都内の全高校の進学実績を取得

--- tokyo_highschool/spiders/schoolinfo.py
+++ tokyo_highschool/spiders/schoolinfo.py
@@ -16,9 +16,17 @@ class SchoolinfoSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_item)
 
     def parse_item(self, response):
+        university = []
+        for row in response.css('#main > div.sch-detail-main-box > div.sch-detail-goukakujisseki > table > tr'):
+            univ = row.css('td.goukakujisseki-school > a::text').get()
+            if univ:
+                pass2021 = "0" if row.css('td:nth-child(4)::text').get().replace(" ","").replace("人","").strip() in ["-",""] else row.css('td:nth-child(4)::text').get().replace(" ","").replace("人","").strip()
+                pass2020 = "0" if row.css('td:nth-child(5)::text').get().replace(" ","").replace("人","") in ["-",""] else row.css('td:nth-child(5)::text').get().replace(" ","").replace("人","")
+                pass2019 = "0" if row.css('td:nth-child(6)::text').get().replace(" ","").replace("人","") in ["-",""] else row.css('td:nth-child(6)::text').get().replace(" ","").replace("人","")
+                university.append([univ,pass2019,pass2020,pass2021])
         yield {
             'highschool':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-top > div.mod-school-name::text').get(),
             'schoolcaption':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-top > div.mod-school-caption::text').get().replace("(","").replace(")",""),
             'hensa':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-bottom > div.mod-school-info > p > span::text').get(),
-            'university':response.css('#main > div.sch-detail-main-box > div.sch-detail-goukakujisseki > table > tbody > tr:nth-child(3) > td.goukakujisseki-school > a::attr(href)').get(),
+            'achievement':university,
         }
