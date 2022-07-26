9b519b86a118cfc9a4f2d1ff3fd18a24fd5487bc
oden6680 <oden6680@gmail.com>
2022-07-26 10:07:01 +0900

    全件取得するプログラムに変更

--- tokyo_highschool/spiders/schoolinfo.py
+++ tokyo_highschool/spiders/schoolinfo.py
@@ -1,20 +1,24 @@
 import scrapy
-
+import re
 
 class SchoolinfoSpider(scrapy.Spider):
     name = 'schoolinfo'
     allowed_domains = ['www.minkou.jp']
-    start_urls = ['https://www.minkou.jp/hischool/search/pref=tokyo/']
+    pages_number = 23
+    start_urls = []
+    for i in range(1,pages_number+1):
+        start_urls.append('https://www.minkou.jp/hischool/search/page='+str(i)+'/pref=tokyo/')
 
     def parse(self, response):
         for href in response.css('#container > #contents > #main > #under > div.mod-listSearch > ul > li > a::attr(href)'):
-            url = 'https://www.minkou.jp//hischool/school/university' + href.get().replace("/hischool/school","")
+            schoolId = href.get().replace("/hischool/school/","").replace("interview/","")
+            url = 'https://www.minkou.jp/hischool/school/university/' + re.sub('/\d{2,}',"/",schoolId)
             yield scrapy.Request(url, callback=self.parse_item)
 
     def parse_item(self, response):
         yield {
-            'highschool':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-top > h1.mod-school-name::text').get(),
+            'highschool':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-top > div.mod-school-name::text').get(),
             'schoolcaption':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-top > div.mod-school-caption::text').get().replace("(","").replace(")",""),
             'hensa':response.css('#container > #contents > #main > div.mod-school > div.mod-school-inner > div.mod-school-r > div.mod-school-bottom > div.mod-school-info > p > span::text').get(),
-            'university': response.css('#main > div.sch-detail-main-box > div.sch-detail-goukakujisseki > table > tbody > tr:nth-child(3) > td.goukakujisseki-school > a::text').get(),
+            'university':response.css('#main > div.sch-detail-main-box > div.sch-detail-goukakujisseki > table > tbody > tr:nth-child(3) > td.goukakujisseki-school > a::attr(href)').get(),
         }
